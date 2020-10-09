88c7be93099d588ff43e7560d2835d22dca817ae
geumjaLee <polina.fedorova@devexpress.com>
2020-10-09 16:04:10 +0300

    new annotation API

--- CS/CreateMarkupAnnotation/Program.cs
+++ CS/CreateMarkupAnnotation/Program.cs
@@ -1,31 +1,106 @@
-﻿using System;
-using DevExpress.Pdf;
+﻿using DevExpress.Pdf;
+using System.Linq;
 
-namespace CreateMarkupAnnotation {
-    class Program {
-        static void Main(string[] args) {
+namespace PdfMarkupAnnotations
+{
+    class Program
+    {
 
-            // Create a PDF Document Processor.
-            using (PdfDocumentProcessor processor = new PdfDocumentProcessor()) {
 
-                // Load a document.
+        static void Main(string[] args)
+        {
+            using (PdfDocumentProcessor processor = new PdfDocumentProcessor())
+            {
+                //Load a document:
                 processor.LoadDocument("..\\..\\Document.pdf");
+                CreateAnnotations(processor);
+                EditAnnotations(processor);
+                DeleteAnnotations(processor);
 
-                // Add a text markup annotation to the first page for a text corresponding to the specified page area.
-                PdfTextMarkupAnnotationData annot = processor.AddTextMarkupAnnotation(1, new PdfRectangle(90, 100, 240, 230),
-                                            PdfTextMarkupAnnotationType.Highlight);
-                if (annot != null) {
+                //Save the result:
+                processor.SaveDocument("..\\..\\Result.pdf");
+            }
+            System.Diagnostics.Process.Start("..\\..\\Result.pdf");
+        }
 
-                    // Specify the annotation properties.                    
-                    annot.Author = "Bill Smith";
-                    annot.Contents = "Note";
-                    annot.Color = new PdfRGBColor(0.8, 0.2, 0.1);
 
-                    // Save the result document.
-                    processor.SaveDocument("..\\..\\Result.pdf");
-                }
-                else
-                    Console.WriteLine("The annotation cannot be added to a page. Make sure, a specified page area corresponds to a text on the page.");
+
+        private static void CreateAnnotations(PdfDocumentProcessor processor)
+        {
+            //Add a text markup annotation at the first page:
+            PdfDocumentPosition startPosition = new PdfDocumentPosition(1, new PdfPoint(113, 224));
+            PdfDocumentPosition endPosition = new PdfDocumentPosition(1, new PdfPoint(272, 207));
+
+            PdfTextMarkupAnnotationData textMarkupAnnotation = processor.AddTextMarkupAnnotation(startPosition,endPosition,
+                                        PdfTextMarkupAnnotationType.Highlight);
+            if (textMarkupAnnotation != null)
+            {
+                //Specify the annotation properties:
+                textMarkupAnnotation.Author = "Bill Smith";
+                textMarkupAnnotation.Subject = "Important!";
+                textMarkupAnnotation.Contents = "Please, fact-check this diagram";
+                textMarkupAnnotation.Color = new PdfRGBColor(0.10, 0.85, 1.00);
+                AddAnnotationComments(textMarkupAnnotation);
+            }
+
+            //Add a sticky note at the first page:
+            PdfTextAnnotationData textAnnotation = processor.AddTextAnnotation(1, new PdfPoint(64, 65));
+
+            //Specify the annotation parameters:
+            textAnnotation.Author = "Nancy Davolio";
+            textAnnotation.Color = new PdfRGBColor(0.8, 0.2, 0.1);
+            textAnnotation.Contents = "Please proofread this document";
+            textAnnotation.IconName = PdfTextAnnotationIconName.Check;
+            AddAnnotationComments(textAnnotation);
+
+        }
+        private static void EditAnnotations(PdfDocumentProcessor processor)
+        {
+            //Retrieve annotations made by the specified author:
+            var textMarkups =
+                processor.GetMarkupAnnotationData(1).Where(annotation => annotation.Author.Contains("Cardle Anita L"));
+            foreach (PdfMarkupAnnotationData markup in textMarkups)
+            {
+                //Get all text markup annotations from the retrieved list:
+                PdfTextMarkupAnnotationData pdfTextMarkup = markup.AsTextMarkupAnnotation();
+                if (pdfTextMarkup != null)
+                    //Change the annotation's markup type:
+                    pdfTextMarkup.MarkupType = PdfTextMarkupAnnotationType.Squiggly;
+            }
+
+            var annotations = processor.GetMarkupAnnotationData(1);
+            foreach (PdfMarkupAnnotationData annotation in annotations)
+            {
+                //Get all text annotations:
+                PdfTextAnnotationData textAnnotation = annotation.AsTextAnnotation();
+                if (textAnnotation != null)
+
+                    //Change the annotation icon:
+                    textAnnotation.IconName = PdfTextAnnotationIconName.Note;
+            }
+            annotations[0].AddReview("Borman Aaron Lewis", PdfAnnotationReviewStatus.Completed);
+        }
+
+
+        private static void AddAnnotationComments(PdfMarkupAnnotationData annotation)
+        {
+            //Add a comment for the specified annotation:
+            PdfMarkupAnnotationComment comment = annotation.AddReply("Reviewer", "Done");
+            comment.Subject = "Proofread";
+
+            //Create a nested comment and add a review:
+            PdfMarkupAnnotationComment nestedComment = comment.AddReply(annotation.Author, "Thanks");
+            nestedComment.Subject = "Reviewed";
+            nestedComment.AddReview("Reviewer", PdfAnnotationReviewStatus.Accepted);
+        }
+
+        private static void DeleteAnnotations(PdfDocumentProcessor processor)
+        {
+            for (int i = 0; i <= processor.Document.Pages.Count; i++)
+            {
+                //Borman Aaron Lewis's markup annotations from a page.
+                processor.DeleteMarkupAnnotations(processor.GetMarkupAnnotationData(i)
+                .Where(annotation => annotation.Author.Contains("Borman Aaron Lewis")));
             }
         }
     }
--- VB/CreateMarkupAnnotation/Program.vb
+++ VB/CreateMarkupAnnotation/Program.vb
@@ -1,31 +1,94 @@
-﻿Imports System
-Imports DevExpress.Pdf
+﻿Imports DevExpress.Pdf
+Imports System.Linq
 
-Namespace CreateMarkupAnnotation
-    Friend Class Program
-        Shared Sub Main(ByVal args() As String)
+Namespace PdfMarkupAnnotations
+	Friend Class Program
 
-            ' Create a PDF Document Processor.
-            Using processor As New PdfDocumentProcessor()
 
-                ' Load a document.
-                processor.LoadDocument("..\..\Document.pdf")
+		Shared Sub Main(ByVal args() As String)
+			Using processor As New PdfDocumentProcessor()
+				'Load a document:
+				processor.LoadDocument("..\..\Document.pdf")
+				CreateAnnotations(processor)
+				EditAnnotations(processor)
+				DeleteAnnotations(processor)
 
-                ' Add a text markup annotation to the first page for a text corresponding to the specified page area.
-                Dim annot As PdfTextMarkupAnnotationData = processor.AddTextMarkupAnnotation(1, New PdfRectangle(90, 100, 240, 230), PdfTextMarkupAnnotationType.Highlight)
-                If annot IsNot Nothing Then
+				'Save the result:
+				processor.SaveDocument("..\..\Result.pdf")
+			End Using
+			System.Diagnostics.Process.Start("..\..\Result.pdf")
+		End Sub
 
-                    ' Specify the annotation properties.                    
-                    annot.Author = "Bill Smith"
-                    annot.Contents = "Note"
-                    annot.Color = New PdfRGBColor(0.8, 0.2, 0.1)
 
-                    ' Save the result document.
-                    processor.SaveDocument("..\..\Result.pdf")
-                Else
-                    Console.WriteLine("The annotation cannot be added to a page. Make sure, a specified page area corresponds to a text on the page.")
-                End If
-            End Using
-        End Sub
-    End Class
+
+		Private Shared Sub CreateAnnotations(ByVal processor As PdfDocumentProcessor)
+			'Add a text markup annotation at the first page:
+			Dim startPosition As New PdfDocumentPosition(1, New PdfPoint(113, 224))
+			Dim endPosition As New PdfDocumentPosition(1, New PdfPoint(272, 207))
+
+			Dim textMarkupAnnotation As PdfTextMarkupAnnotationData = processor.AddTextMarkupAnnotation(startPosition,endPosition, PdfTextMarkupAnnotationType.Highlight)
+			If textMarkupAnnotation IsNot Nothing Then
+				'Specify the annotation properties:
+				textMarkupAnnotation.Author = "Bill Smith"
+				textMarkupAnnotation.Subject = "Important!"
+				textMarkupAnnotation.Contents = "Please, fact-check this diagram"
+				textMarkupAnnotation.Color = New PdfRGBColor(0.10, 0.85, 1.00)
+				AddAnnotationComments(textMarkupAnnotation)
+			End If
+
+			'Add a sticky note at the first page:
+			Dim textAnnotation As PdfTextAnnotationData = processor.AddTextAnnotation(1, New PdfPoint(64, 65))
+
+			'Specify the annotation parameters:
+			textAnnotation.Author = "Nancy Davolio"
+			textAnnotation.Color = New PdfRGBColor(0.8, 0.2, 0.1)
+			textAnnotation.Contents = "Please proofread this document"
+			textAnnotation.IconName = PdfTextAnnotationIconName.Check
+			AddAnnotationComments(textAnnotation)
+
+		End Sub
+		Private Shared Sub EditAnnotations(ByVal processor As PdfDocumentProcessor)
+			'Retrieve annotations made by the specified author:
+			Dim textMarkups = processor.GetMarkupAnnotationData(1).Where(Function(annotation) annotation.Author.Contains("Cardle Anita L"))
+			For Each markup As PdfMarkupAnnotationData In textMarkups
+				'Get all text markup annotations from the retrieved list:
+				Dim pdfTextMarkup As PdfTextMarkupAnnotationData = markup.AsTextMarkupAnnotation()
+				If pdfTextMarkup IsNot Nothing Then
+					'Change the annotation's markup type:
+					pdfTextMarkup.MarkupType = PdfTextMarkupAnnotationType.Squiggly
+				End If
+			Next markup
+
+			Dim annotations = processor.GetMarkupAnnotationData(1)
+			For Each annotation As PdfMarkupAnnotationData In annotations
+				'Get all text annotations:
+				Dim textAnnotation As PdfTextAnnotationData = annotation.AsTextAnnotation()
+				If textAnnotation IsNot Nothing Then
+
+					'Change the annotation icon:
+					textAnnotation.IconName = PdfTextAnnotationIconName.Note
+				End If
+			Next annotation
+			annotations(0).AddReview("Borman Aaron Lewis", PdfAnnotationReviewStatus.Completed)
+		End Sub
+
+
+		Private Shared Sub AddAnnotationComments(ByVal annotation As PdfMarkupAnnotationData)
+			'Add a comment for the specified annotation:
+			Dim comment As PdfMarkupAnnotationComment = annotation.AddReply("Reviewer", "Done")
+			comment.Subject = "Proofread"
+
+			'Create a nested comment and add a review:
+			Dim nestedComment As PdfMarkupAnnotationComment = comment.AddReply(annotation.Author, "Thanks")
+			nestedComment.Subject = "Reviewed"
+			nestedComment.AddReview("Reviewer", PdfAnnotationReviewStatus.Accepted)
+		End Sub
+
+		Private Shared Sub DeleteAnnotations(ByVal processor As PdfDocumentProcessor)
+			For i As Integer = 0 To processor.Document.Pages.Count
+				'Borman Aaron Lewis's markup annotations from a page.
+				processor.DeleteMarkupAnnotations(processor.GetMarkupAnnotationData(i).Where(Function(annotation) annotation.Author.Contains("Borman Aaron Lewis")))
+			Next i
+		End Sub
+	End Class
 End Namespace
