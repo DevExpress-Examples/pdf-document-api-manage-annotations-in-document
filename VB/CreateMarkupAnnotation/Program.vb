220358eb650045315d801f1905dae87bf76bb45a
DevExpressExampleBot <fakeEmail>
2022-02-01 21:43:17 +0300

    Source auto update [skip ci]

--- VB/CreateMarkupAnnotation/Program.vb
+++ VB/CreateMarkupAnnotation/Program.vb
@@ -1,25 +1,22 @@
-﻿Imports System
+Imports System
 Imports DevExpress.Pdf
 
 Namespace CreateMarkupAnnotation
+
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
+        Shared Sub Main(ByVal args As String())
             ' Create a PDF Document Processor.
-            Using processor As New PdfDocumentProcessor()
-
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Load a document.
                 processor.LoadDocument("..\..\Document.pdf")
-
                 ' Add a text markup annotation to the first page for a text corresponding to the specified page area.
                 Dim annot As PdfTextMarkupAnnotationData = processor.AddTextMarkupAnnotation(1, New PdfRectangle(90, 100, 240, 230), PdfTextMarkupAnnotationType.Highlight)
                 If annot IsNot Nothing Then
-
                     ' Specify the annotation properties.                    
                     annot.Author = "Bill Smith"
                     annot.Contents = "Note"
                     annot.Color = New PdfRGBColor(0.8, 0.2, 0.1)
-
                     ' Save the result document.
                     processor.SaveDocument("..\..\Result.pdf")
                 Else
